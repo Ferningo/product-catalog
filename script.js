e2828bf9c9697bc7b3533aab001e847f24170c8d
Jose <jose.mar.123@hotmail.com>
2023-02-07 13:41:19 -0600

    Add functtion to populate mermelade section
    -Added a function to dinamically insert mermelade card items.
    
    -Added necesary sections to the HTML and included additonal images.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const myFunc = () => {
+const addSauce = () => {
   const sauceRow = document.getElementById("sauces-row");
-  const sauceCardHtml = createSauceCard();
+  const sauceCardHtml = createSauceCard("sauce");
   sauceRow.innerHTML += sauceCardHtml;
 };
 
-const createSauceCard = () => {
+const addMermelade = () => {
+  const sauceRow = document.getElementById("mermelades-row");
+  const sauceCardHtml = createSauceCard("mermelade");
+  sauceRow.innerHTML += sauceCardHtml;
+};
+
+const createSauceCard = (type) => {
+  let imgRoute;
+  let cardTitle;
+
+  if (type === "sauce") {
+    imgRoute = "./assets/img/sauce_1.png";
+    cardTitle = "Salsa de chile morita";
+  } else {
+    imgRoute = "./assets/img/mermelade_1.png";
+    cardTitle = "Ponche de frutas";
+  }
+
   return `
-    <div class="col-4">
+    <div class="col-sm-4">
     <div class="card text-center border-0" style="width: 18rem">
-      <img src="img/sauce.png" class="card-img-top" alt="..." />
+      <img src=${imgRoute} class="card-img-top" alt="..." />
       <div class="card-body">
-        <h5 class="card-title">Salsa 2</h5>
+        <h5 class="card-title">${cardTitle}</h5>
         <a
           href="#"
           class="btn btn-primary btn-lg border-0 btn-rounded"
@@ -22,3 +39,11 @@ const createSauceCard = () => {
     </div>
   </div>`;
 };
+
+async function test() {
+  const resp = await fetch("./data.json");
+  return resp;
+}
+
+const myJson = test();
+console.log(myJson);
